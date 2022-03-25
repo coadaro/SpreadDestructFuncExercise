b9db3223634c51043efd9aa0a51b65bc8117b233
“coadaro” <coadaro@gmail.com>
2022-03-25 01:13:26 -0400

    SpreadDestructFuncExercise

---
+++ script.js
@@ -0,0 +1,27 @@
+//1a
+jPDinos = [
+    "Velociraptors", 
+    "Tyrannosaurus Rex", 
+     "Dilophosaurus"
+];
+
+//1b
+function seeDinos(a, ...b) {
+    console.log(a);
+    console.log(b);
+};
+seeDinos("Velociraptors", "Tyrannosaurus Rex", "Dilophosaurus");
+
+//2a
+const jPCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+};
+
+//2b
+seeCharacters = (ellieSattler) => console.log(ellieSattler);
+seeCharacters(jPCharacters);
+
+//bonus
+//3a
